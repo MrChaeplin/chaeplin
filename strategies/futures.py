56cb1e7a37993e63006718854b9effea3589fe4a
MrChaeplin <yachaejamdong@gmail.com>
2025-07-04 01:22:40 +0900

    chore: add module imports

---
+++ strategies/futures.py
@@ -0,0 +1,8 @@
+from exchange.binance.binance import BinanceFutures
+from exchange.binance.symbols import BTC, ETH
+from exchange.binance.abstract import AbstractAPI
+
+# api = BinanceFutures()
+# balance = api.get_wallet_balance()
+# bitcoin = BTC(api)
+# ethereum = ETH(api)
---
+++ strategies/spot.py
@@ -0,0 +1,8 @@
+from exchange.binance import BinanceSpot, BTC, ETH
+from exchange.binance.symbols import BTC, ETH
+from exchange.binance.abstract import AbstractAPI
+
+# api = BinanceSpot()
+# balance = api.get_wallet_balance()
+# bitcoin = BTC(api)
+# ethereum = ETH(api)
