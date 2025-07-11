8524f1ad2f425316a7f6162b71e7d688511e05b4
MrChaeplin <yachaejamdong@gmail.com>
2025-07-11 18:24:04 +0900

    refactor: apply safe order execution and unify trader interface with futures

--- exchange/binance/spot.py
+++ exchange/binance/spot.py
@@ -1,53 +1,53 @@
 from exchange.binance.api import BinanceAPI
+from utils import errors
+from exchange.base.base_trader import BaseTrader
 
 
 class BinanceSpot(BinanceAPI):  # raw wrapper
     def __init__(self):
         super().__init__("spot")
 
+    def fetch_ohlcv(self, symbol: str, timeframe="1m", limit=60):
+        return self._get_api().fetch_ohlcv(symbol, timeframe=timeframe, limit=limit)
+
     def get_wallet_balance(self):
         """Fetch spot wallet balance"""
         return self._get_api().fetch_balance()
 
-    def buy_market(self, symbol, amount):
+    def get_order_history(self, symbol: str):
+        return self._get_api().private_get_allorders(
+            {"symbol": symbol.replace("/", "")}
+        )
+
+    def buy_market(self, symbol: str, amount: float):
         """Buy at market price // 시장가 매수"""
-        return self._get_api().create_order(symbol, "market", "buy", amount)
+        return errors.safe_create_order(
+            self._get_api(), symbol, "market", "buy", amount
+        )
 
-    def buy_limit(self, symbol, amount, price):
+    def buy_limit(self, symbol: str, amount: float, price: float):
         """Buy position at limit price // 지정가 매수"""
-        return self._get_api().create_order(symbol, "limit", "buy", amount, price=price)
+        return errors.safe_create_order(
+            self._get_api(), symbol, "limit", "buy", amount, price=price
+        )
 
-    def sell_market(self, symbol, amount):
+    def sell_market(self, symbol: str, amount: float):
         """Sell position at market price // 시장가 매도"""
-        return self._get_api().create_order(symbol, "market", "sell", amount)
+        return errors.safe_create_order(
+            self._get_api(), symbol, "market", "sell", amount
+        )
 
-    def sell_limit(self, symbol, amount, price):
+    def sell_limit(self, symbol: str, amount: float, price: float):
         """Sell position at limit price // 지정가 매도"""
-        return self._get_api().create_order(
-            symbol, "limit", "sell", amount, price=price
+        return errors.safe_create_order(
+            self._get_api(), symbol, "limit", "sell", amount, price=price
         )
 
 
-class SpotTrader:
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+class SpotTrader(BaseTrader):
+
     def __init__(self, api: BinanceSpot, symbol: str):
-        self.api = api
-        self.symbol = symbol
-
-    def fetch_ohlcv(self, timeframe="1m", limit=60):
-        return self.api._get_api().fetch_ohlcv(
-            self.symbol, timeframe=timeframe, limit=limit
-        )
-
-
-class BTCSpot(SpotTrader):
-    """SpotTrader for BTC/USDT only"""
-
-    def __init__(self, api: BinanceSpot):
-        super().__init__(api, "BTC/USDT")
-
-
-class ETHSpot(SpotTrader):
-    """SpotTrader for ETH/USDT only"""
-
-    def __init__(self, api: BinanceSpot):
-        super().__init__(api, "ETH/USDT")
+        super().__init__(api, symbol)
